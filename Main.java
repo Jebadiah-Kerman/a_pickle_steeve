aa3ef36bcb0eb1b6946528fd4c3854482d5fde93
Jebadiah-Kerman <142602441+Jebadiah-Kerman@users.noreply.github.com>
2023-09-22 17:07:49 +0000

    added more to steve

--- Main.java
+++ Main.java
@@ -3,8 +3,8 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        System.out.print("It's time to play family feud! steve harvey"  + '\n');
-        System.out.print(";;;;;;;;;:,     .,,,,,:::;;;;;iiiiiiiiiiiiiiiifLLLLt;fLLLLt;tLLLLt1t11111111fLLLLf1i;;;tCLLLL1;iii;;"  + '\n' );
+        System.out.print("It's time to play family feud!"  + '\n');
+        System.out.print(";;;;;;;;;:,     .,,,,,:::;;;;;iiiiiiiiiiiiiiiifLLLLt;fLLLLt;tLLLLt1t11111111fLLLLf1i;;;tCLLLL1;iii;;"  + '\n' ";;;;;;;;::,     .,,,,,:::;;;;;iiiii;iiiiiii111tfffLt;tLLLLf;1LLLLtitt11iiiiifLLLLf1111itLLLLLt1111i;" + '\n');
         System.out.println("What does HT stand for?");
        
         String answer = scan.nextLine();
